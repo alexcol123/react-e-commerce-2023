b77455083bf42ca584bae71275e112c9d57a053f
henry  munoz <henrymunoz@henrys-Mac-mini.local>
2023-02-08 16:24:32 -0500

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Menu from './components/Menu';
+import Menu from './components/Menu'
 
 function App() {
   return (
     <BrowserRouter>
-    <Menu />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
+      <Menu />
+      <div style={{ marginTop: '80px' }}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+  return     <div className='container'>
+  Register ipsum dolor sit, amet consectetur adipisicing elit. Natus vitae
+  veniam praesentium doloremque repudiandae atque quo, hic voluptatum optio
+  id cum. Quisquam, exercitationem ex! Error eaque voluptatem ea perferendis
+  esse deleniti vel delectus placeat doloremque nisi officia quos iste
+  rerum, quis ut nesciunt aut inventore quam harum earum officiis pariatur.
+  Aliquam hic earum impedit, atque architecto voluptates laboriosam soluta
+  itaque placeat in dolorem voluptate veniam cupiditate et tempore nam sed
+  repellat quas sit doloremque voluptatum ipsum praesentium! Error, ipsum
+  quo eum debitis vel molestiae dolorum reiciendis laboriosam ducimus
+  praesentium laudantium delectus, aut architecto non dolorem accusantium.
+  Iusto voluptatem praesentium distinctio.
+</div>
 }
 
 export default Register
