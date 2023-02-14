70b088126c1e9b48264d628c0036071df9fa6d64
henry  munoz <henrymunoz@henrys-Mac-mini.local>
2023-02-14 15:03:49 -0500

    started working on page for admin to create products

--- client/src/pages/admin/AdminProduct.js
+++ client/src/pages/admin/AdminProduct.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 import Jumbotron from '../../components/cards/Jumbotron'
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '../../context/auth'
@@ -6,6 +9,30 @@ import AdminMenu from '../../components/nav/AdminMenu'
 const AdminProduct = () => {
   // Context
   const [auth, setAuth] = useAuth()
+  // State
+  const [categories, setCategories] = useState([])
+
+  const [photo, setPhoto] = useState('')
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+  const [category, setCategory] = useState('Choose a Category')
+  console.log(category)
+  const [shipping, setShipping] = useState('')
+  const [quantity, setQuantity] = useState('')
+
+  useEffect(() => {
+    loadCategories()
+  }, [])
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get('/categories')
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
@@ -24,10 +51,23 @@ const AdminProduct = () => {
           {/* Main */}
           <div className='col-md-9'>
             <div className='p-3 mb-2  border mt-1'>
-              <h4 className='text-center '>Create Product</h4>
+              <h4 className='text-center '>Create Product </h4>
             </div>
 
-            <h3>Crete Product Form</h3>
+            {/* select */}
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className='form-select'
+            >
+              <option>{category}</option>
+
+              {categories.map((c) => (
+                <option value={c.name} key={c._id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
